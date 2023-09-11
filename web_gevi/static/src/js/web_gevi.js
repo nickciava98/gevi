694e8d4ed265a4450295510df4100a7caf13c57a
nickciava98 <n.ciavarella@outlook.it>
2023-09-11 15:08:43 +0200

    Porting 16.0

--- web_gevi/static/src/js/web_gevi.js
+++
@@ -1,8 +0,0 @@
-openerp.web_gevi = function(instance) {
-    instance.web.WebClient.include({
-        init: function(parent, client_options) {
-            this._super(parent, client_options);
-            this.set('title_part', {"zopenerp": "Gevi"});
-        },
-    });
-};
